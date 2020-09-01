43f48a6e3213c05c213f61cd2a0c52194ffc3c8f
Anthony Sylvester <anthony.sylvester99@gmail.com>
2020-08-31 19:41:04 -0700

    adds global styles

---
+++ src/global-styles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #000000;
+    color: ;
+    font-size: 1rem;
+    margin: 0;
+  }
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
