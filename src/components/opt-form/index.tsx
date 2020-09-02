37d2fcc4278fecb5fad8bc44a2efd659e207dafa
Anthony Sylvester <anthony.sylvester99@gmail.com>
2020-09-02 14:28:38 -0700

    styles opt in form

--- src/components/opt-form/index.tsx
+++ src/components/opt-form/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import { Container, Input, Button, Text } from './styles/opt-form';
+import chevronPng from '../../images/icons/chevron-right.png';
+import { Container, Input, Button, Text, Break } from './styles/opt-form';
 
 interface IRestProps {
   [rest: string]: any;
@@ -9,6 +10,7 @@ interface IComposition {
   Input: FC<IRestProps>;
   Button: FC<IRestProps>;
   Text: FC<IRestProps>;
+  Break: FC<IRestProps>;
 }
 
 export const OptForm: FC<IRestProps> & IComposition = ({
@@ -25,9 +27,13 @@ OptForm.Input = function OptFormInput({ ...restProps }) {
 OptForm.Button = function OptFormButton({ children, ...restProps }) {
   return (
     <Button {...restProps}>
-      {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
+      {children} <img src={chevronPng} alt="Try Now" />
     </Button>
   );
+};
+
+OptForm.Break = function OptBreak({ ...restProps }) {
+  return <Break {...restProps} />;
 };
 
 OptForm.Text = function OptFormText({ children, ...restProps }) {
